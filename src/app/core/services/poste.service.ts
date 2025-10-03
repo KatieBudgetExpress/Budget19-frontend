39602eabc3437a30ff01f2b43147e76000d52086
KatieBudgetExpress <katie@budget-express.com>
2025-10-03 19:27:15 -0400

    Update poste.service.ts

--- src/app/core/services/poste.service.ts
+++ src/app/core/services/poste.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {
-  CreatePostePayload,
-  PosteBudgetaire,
-  UpdatePostePayload,
-} from '../models/poste.model';
+
+export interface Poste {
+  id: number;
+  name: string;
+  description?: string;
+  amount: number;
+  category: string;
+  dateDebut?: string;
+  dateFin?: string;
+}
+
+export type PosteBudgetaire = Poste;
+export type CreatePostePayload = Omit<Poste, 'id'>;
+export type UpdatePostePayload = Partial<CreatePostePayload>;
 
 @Injectable({
   providedIn: 'root',
